840d8cda9020c2bc960b2e48468c852c87cdc386
steven dale <lifeinchords@gmail.com>
2017-01-02 19:56:20 -0500

    fixing wrong lib npm install. basic mojs test based on https://codepen.io/rkhayat/pen/aBpMRr

--- components/Paragraph/Paragraph.js
+++ components/Paragraph/Paragraph.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import mojs from 'mo-js';
+// import MojsPlayer from 'mojs-player';
+// import MojsCurveEditor from 'mojs-curve-editor';
+
 
 class Paragraph extends React.Component {
+
+
+	componentDidMount(){
+	  document.addEventListener( 'click' , (e) => {
+	     burst
+	         .tune({ x: e.pageX, y: e.pageY })
+	         .replay();
+	  });
+	}
+
   render() {
     return (<p>{this.props.text}</p>);
   }
 }
 
 export default Paragraph;
+
+
+
+const burst = new mojs.Burst({
+  left: 0, top: 0,
+  radius:   { 4: 19 },
+  angle:    45,
+  children: {
+    shape:        'line',
+    radius:       3,
+    scale:        1,
+    stroke:       '#FD7932',
+    strokeDasharray: '100%',
+    strokeDashoffset: { '-100%' : '100%' },
+    duration:     700,
+    easing:       'quad.out',
+  }
+});
